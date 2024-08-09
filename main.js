9965b912520cf22f0d5e1b32c9f4479b8b9cb8cf
DanielDeanGithub <github-new@inboxrefresh.com>
2024-08-09 08:20:57 +0100

    Update code to remove comments

--- main.js
+++ main.js
@@ -31,33 +31,13 @@ class Game {
         this._player = [0,0];
     }
     
-    get map() {
-        return this._map;
-    }
-
-    get gameRun() {
-        return this._gameRun;
-    }
-
-    set gameRun(gameRun) {
-        this._gameRun = gameRun;
-    }
-
-    get player() {
-        return this._player;
-    }
-
-    set player(player) {
-        this._player = player;
-    }
-
-    get preview() {
-        return this._preview;
-    }
-
-    set preview(preview) {
-        this._preview = preview;
-    }
+    get map() { return this._map; }
+    get gameRun() { return this._gameRun; }
+    set gameRun(gameRun) { this._gameRun = gameRun; }
+    get player() { return this._player; }
+    set player(player) { this._player = player; }
+    get preview() { return this._preview; }
+    set preview(preview) { this._preview = preview; }
 
     resetPreview() {
         return this.preview.field.forEach((row, i) => {       
@@ -71,36 +51,22 @@ class Game {
 
     playGame() {
         this.resetPreview();
-        console.log('Time to find your hat!');
-        console.log(`To begin please enter a direction (type 'W' for up, 'A' for left, 'S' for down or 'D' for right)\n`); 
+        console.log(`Time to find your hat!\nTo begin please enter a direction (type 'W' for up, 'A' for left, 'S' for down or 'D' for right)`); 
 
+        this.preview.print();
         const mapArr = this.map.field;
-
-        //console.log(this.map);
-        //console.log(this.preview);
-        //console.log(this.gameRun);
         this.gameRun = true;
-        //console.log(this.gameRun);
-
-        //this.map.print()
-        //console.log(mapArr.length);
 
         while(this.gameRun) {
             let userInput = prompt('Please enter a direction to explore:').toUpperCase();
-
+            
             while (userInput !== 'W' && userInput !== 'A' && userInput !== 'S' && userInput !== 'D') {
                 console.log(`Error. Please enter a valid direction ('W','A','S', or 'D')`);
                 userInput = prompt('Please enter a direction to explore:').toUpperCase();
             }
 
-
-            //console.log(userInput);
-
             let x = this.player[0];
             let y = this.player[1];
-
-            //console.log(`x: ${x} - y: ${y}`);
-
             
             switch (userInput) {
                 case 'W':
@@ -119,14 +85,8 @@ class Game {
                     break;
             }
 
-            //console.log(`x: ${x} - y: ${y}`);
-            //console.log(mapArr.length);
-            //console.log(mapArr[y].length);
-
             // error checking to make sure movement is within bounds
             if (y > 0 && y < mapArr.length || x > 0 && x < mapArr[y].length) {
-
-                //console.log(this.player);
                 if (mapArr[y][x] === hole) {
                     console.log('You fell down a hole. Game over =[');
                     return this.gameRun = false;
@@ -139,26 +99,15 @@ class Game {
 
                 console.log('The search continues...')
                 this.player = [x,y];
-                
-                
-                //console.log(this.map.field[y][x]);
-               
-                //this.map.print();
+                this.preview.field[y][x] = pathCharacter;
                 this.preview.print();
-                //console.log(this.player);
 
             } else {
                 console.log('Ouch! You just walked into a walk.');
-            }
-            //this.map.print();            
+            }          
         }
     }
-
-
-
 };
-
-
 
 const myField = new Field([
     ['*', '░', 'O'],
@@ -167,8 +116,4 @@ const myField = new Field([
 ]);
 
 const hatGame = new Game(myField);
-
-
 hatGame.playGame();
-
-//hatGame.field.print();
